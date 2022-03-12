13c93f0a590dea9221f3816f5d1058787f0c91e1
haritha175 <93146787+haritha175@users.noreply.github.com>
2022-03-12 21:46:01 +0530

    this is basic example on java datatypes

---
+++ Datatypes.java
@@ -0,0 +1,30 @@
+public class Datatypes
+{
+public static void main(String args[])
+{
+ byte a;
+ short b;
+ int c;
+ long d;
+  float e;
+  double f;
+   char g;
+   boolean h;
+ a = 127;
+ b = 32767;
+ c = 2147483647;
+ d = 9223372036854775807l;
+ e = 1.1f;
+ f =1565321.156653;
+ g ='h';
+ h = true;
+System.out.println(" the value of byte is"+a);
+System.out.println(" the value of short is"+b);
+System.out.println(" the value of int is"+c);
+System.out.println(" the value of long is"+d);
+System.out.println(" the value of double is"+e);
+System.out.println(" the value of float is"+f);
+System.out.println(" the value of char is"+g);
+System.out.println(" the value of boolean is"+h);
+}
+}
